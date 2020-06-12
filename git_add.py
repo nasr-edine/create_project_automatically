0cf1f95289abd8d6a634837d3ab02bd7e89e8c5f
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2020-06-12 14:53:07 +0200

    add files git_add.py

--- git_add.py
+++ git_add.py
@@ -2,11 +2,19 @@ import os
 import sys
 
 str = ' '.join(sys.argv[1:])
-
+msgCommit = input("message: ")
 
 print(f'git add {str}')
-print(f'git commit -m "add files: {str}"')
+if not msgCommit:
+    print(f'git commit -m "add files: {str}"')
+else:
+    print(f'git commit -m "add files: {msgCommit}"')
+
 
 os.system(f'git add {str}')
 os.system(f'git commit -m "add files {str}"')
+if not msgCommit:
+    os.system(f'git commit -m "add files: {str}"')
+else:
+    os.system(f'git commit -m "{msgCommit}"')
 os.system('git push origin master')
